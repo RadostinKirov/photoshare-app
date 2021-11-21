9545b2f6a0ca5f8fa5a390994dbd45c23c02b7f2
RadostinKirov <74134419+RadostinKirov@users.noreply.github.com>
2021-11-21 15:10:19 +0200

    404 sad image update

--- src/components/Page404/Page404.js
+++ src/components/Page404/Page404.js
@@ -3,7 +3,7 @@ const Page404 = () => {
         <div class="page404">
             <section>
                 <div className="image">
-                    <img src="./images/404.png" />
+                    <img src="./images/404-sad.png" />
                 </div>
                 <div>
                     <a class="btn-home" >Home</a>
