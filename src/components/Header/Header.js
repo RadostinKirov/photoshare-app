f5a09602e44df90f35416c9e75eae19ae3652bec
RadostinKirov <74134419+RadostinKirov@users.noreply.github.com>
2021-12-03 16:49:23 +0200

    header cond rend added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,6 +5,7 @@ import AuthContext from '../../contexts/AuthContext';
 const Header = ({ navigationChangeHandler }) => {
 
     const { userInfo } = useContext(AuthContext);
+    const isUser = Boolean(userInfo.username.length);
     console.log('userInfo -> ', userInfo);
     return (
         <header>
@@ -16,18 +17,20 @@ const Header = ({ navigationChangeHandler }) => {
             </div>
             <nav>
                 <ul>
+            {!isUser ? 
                     <div className="guest">
                         <li><Link to="/home">HOME</Link></li>
                         <li><Link to="/login">LOGIN</Link></li>
                         <li><Link to="/register">REGISTER</Link></li>
                     </div>
+                    :
                     <div className="user">
                         <li><Link to="/profile" className="hello-user">hello, {userInfo.username}</Link></li>
                         <li><Link to="/catalog">CATALOG</Link></li>
                         <li><Link to="/profile">PROFILE</Link></li>
                         <li><Link to="/create">ADD PHOTO</Link></li>
                         <li><Link to="/logout">LOGOUT</Link></li>
-                    </div>
+                    </div>}
                 </ul>
             </nav>
 
